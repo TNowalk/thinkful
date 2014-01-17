44a6f2ed5570152e2715d28cdeef1c8ac8aee8c2
Trevor Nowalk <trevor.nowalk@twcable.com>
2014-01-16 20:01:14 -0500

    Initial commit.  Added homepage and a couple Python examples.

---
+++ pip/lpthw_ex_1.py
@@ -0,0 +1,15 @@
+'''
+Learn Python the Hard Way
+Exercise 1: A Good First Program
+
+@author Trevor Nowalk <tnowalk@thinkful.com>
+@date 2014-01-16
+'''
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
---
+++ pip/lpthw_ex_2.py
@@ -0,0 +1,17 @@
+'''
+Learn Python the Hard Way
+Exercise 2: Comments and Pound Characters
+
+@author Trevor Nowalk <tnowalk@thinkful.com>
+@date 2014-01-16
+'''
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run."
+
+print "This will run."
