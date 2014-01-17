b6d421fcb43c85598b73028109e314a4af236f73
Trevor Nowalk <trevor.nowalk@twcable.com>
2014-01-16 22:44:53 -0500

    Updating index, changed the link to buttons.  Now has two buttons for direct download and one to view on GitHub

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
 function ProjectsCtrl($scope) {
   $scope.courses = {
-    fewd: {code: 'FEWD-001', name: 'Front-End Web Development'},
-    ios: {code: 'IOS-001', name: 'iOS Development'},
+    //fewd: {code: 'FEWD-001', name: 'Front-End Web Development'},
+    //ios: {code: 'IOS-001', name: 'iOS Development'},
     pip: {code: 'PIP-001', name: 'Programming in Python'},
-    ror: {code: 'ROR-001', name: 'Ruby on Rails'}
+    //ror: {code: 'ROR-001', name: 'Ruby on Rails'}
   };
 
   $scope.projects = {
     fewd: [],
     ios: [],
     pip: [
-      {name: "Exercise 1: A Good First Program", filename: "lpthw_ex_1.py"},
-      {name: "Exercise 2: Comments and Pound Characters", filename: "lpthw_ex_2.py"},
-      {name: "Exercise 11: Asking Questions", filename: "lpthw_ex_11.py"},
-      {name: "Exercise 12: Prompting People", filename: "lpthw_ex_12.py"}
+      {
+        name: "Exercise 1: A Good First Program", 
+        filename: "lpthw_ex_1.py",
+        gitHubUrl: "https://github.com/TNowalk/thinkful/blob/gh-pages/pip/lpthw_ex_1.py"
+      },
+      {
+        name: "Exercise 2: Comments and Pound Characters", 
+        filename: "lpthw_ex_2.py",
+        gitHubUrl: "https://github.com/TNowalk/thinkful/blob/gh-pages/pip/lpthw_ex_2.py"
+      },
+      {
+        name: "Exercise 11: Asking Questions", 
+        filename: "lpthw_ex_11.py",
+        gitHubUrl: "https://github.com/TNowalk/thinkful/blob/gh-pages/pip/lpthw_ex_11.py"
+      },
+      {
+        name: "Exercise 12: Prompting People", 
+        filename: "lpthw_ex_12.py",
+        gitHubUrl: "https://github.com/TNowalk/thinkful/blob/gh-pages/pip/lpthw_ex_12.py"
+      },
+      {
+        name: "Unit 1, Lesson 5: Tip Calculator",
+        filename: "tip_calculator/tip_calculator.py",
+        gitHubUrl: "https://github.com/TNowalk/thinkful/blob/gh-pages/pip/tip_calculator/tip_calculator.py"
+      }
     ],
     ror: []
   };
