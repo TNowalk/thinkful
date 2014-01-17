3e53a2d3e7494f566c1f876897114c7247613f6a
Trevor Nowalk <trevor.nowalk@twcable.com>
2014-01-16 20:05:23 -0500

    More files to init

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function ProjectsCtrl($scope) {
+  $scope.courses = {
+    fewd: {code: 'FEWD-001', name: 'Front-End Web Development'},
+    ios: {code: 'IOS-001', name: 'iOS Development'},
+    pip: {code: 'PIP-001', name: 'Programming in Python'},
+    ror: {code: 'ROR-001', name: 'Ruby on Rails'}
+  };
+
+  $scope.projects = {
+    fewd: [],
+    ios: [],
+    pip: [
+      {name: "Exercise 1: A Good First Program", filename: "lpthw_ex_1.py"},
+      {name: "Exercise 2: Comments and Pound Characters", filename: "lpthw_ex_2.py"}
+    ],
+    ror: []
+  };
+}
