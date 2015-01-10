716af287e532097feff08f543329bab0b5c36dea
Trevor Nowalk <trevor.nowalk@twcable.com>
2015-01-10 02:29:17 -0500

    Adding portfolio

--- js/main.js
+++ js/main.js
@@ -108,6 +108,12 @@ angular.module('ThinkfulProjects')
         type: 'git',
         filename: '1.5.2/index.html',
         gitHubUrl: 'https://github.com/TNowalk/thinkful/blob/gh-pages/fewdv4/1.5.2/index.html'
+      },
+      {
+        name: '1.5.3 - Submitting Your Portfolio',
+        type: 'git',
+        filename: '1.5.3/index.html',
+        gitHubUrl: 'https://github.com/TNowalk/thinkful/blob/gh-pages/fewdv4/1.5.3/index.html'
       }
     ],
     fewd: [
